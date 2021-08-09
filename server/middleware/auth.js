f0ae82fbff1dbab26ae400a7898f599234506015
Sahil Saini <46193454+sahil137@users.noreply.github.com>
2021-08-09 19:38:29 +0530

    add auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.headers.Autherization.split(' ')[1];
+    const isCustomAuth = token.length < 500; // if token length is more than 500 it is google token
+
+    let decodedData;
+
+    if (token && isCustomAuth) {
+      decodedData = jwt.verify(token, 'test');
+
+      req.userId = decodedData?.id;
+    } else {
+      decodedData = jwt.decode(token);
+      req.userId = decodedData?.sub;
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default auth;
